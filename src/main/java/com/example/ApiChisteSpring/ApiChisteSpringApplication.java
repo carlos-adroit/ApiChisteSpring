bd7c201d08e3136891dfbda7fb990f264f4e9ee4
Carlos Verdugo <carlos@adroitoverseas.com>
2025-01-14 12:18:15 -0700

    first commit

---
+++ src/main/java/com/example/ApiChisteSpring/ApiChisteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiChisteSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChisteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChisteSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiChisteSpring/ApiChisteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiChisteSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChisteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
